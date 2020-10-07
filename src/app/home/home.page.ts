46adb0fa7fcad1388de61bdddc91c344b49402e4
sundaran06 <69708797+sundaran06@users.noreply.github.com>
2020-10-07 13:28:04 +0530

    Update home.page.ts
    Reactive form added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  demoForm: FormGroup;
+  submitted = false;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.demoForm = this.formBuilder.group({
+      
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', Validators.required],
+        address: ['', Validators.required]
+    });
+}
+
+// convenience getter for easy access to form fields
+get f() { return this.demoForm.controls; }
+
+onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.demoForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('get value in json!! :-)\n\n' + JSON.stringify(this.demoForm.value, null, 4));
+}
+
+onReset() {
+    this.submitted = false;
+    this.demoForm.reset();
+}
 
 }
